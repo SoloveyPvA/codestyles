b58f22ace553bb1d5513ad2a859c04c862c1b5fc
SBT-Politov-VA <VAPolitov.SBT@sberbank.ru>
2018-09-24 16:20:38 +0500

    add utility class to check assertion

---
+++ src/test/java/ru/pva/code/style/checks/assertions/AssertCheck.java
@@ -0,0 +1,65 @@
+package ru.pva.code.style.checks.assertions;
+
+import static com.puppycrawl.tools.checkstyle.ConfigurationLoader.loadConfiguration;
+import static java.lang.String.format;
+import static java.util.Collections.singletonList;
+import static org.assertj.core.api.Assertions.assertThat;
+import com.puppycrawl.tools.checkstyle.Checker;
+import com.puppycrawl.tools.checkstyle.PropertiesExpander;
+import com.puppycrawl.tools.checkstyle.api.AuditEvent;
+import com.puppycrawl.tools.checkstyle.api.CheckstyleException;
+import java.io.File;
+import java.util.Objects;
+import lombok.val;
+
+public class AssertCheck {
+
+	public static CheckAuditEventAssertion assertThatCheckFor(final String fileConfigPath, final String checkPath)
+			throws CheckstyleException {
+		val checkingDirectory = new File(checkPath);
+		val checker = new Checker();
+		val listener = new StubAuditListener();
+		val config = loadConfiguration(
+				fileConfigPath,
+				new PropertiesExpander(System.getProperties())
+		);
+
+		checker.setModuleClassLoader(Checker.class.getClassLoader());
+		checker.configure(config);
+		checker.addListener(listener);
+
+		val errorCounter = checker.process(singletonList(checkingDirectory));
+
+		return new CheckAuditEventAssertion(listener, errorCounter);
+	}
+
+	public static class CheckAuditEventAssertion {
+
+		private final StubAuditListener listener;
+		private final int errorCount;
+
+		CheckAuditEventAssertion(final StubAuditListener listener, final int errorCounter) {
+			this.errorCount = errorCounter;
+			this.listener = listener;
+		}
+
+		public CheckAuditEventAssertion hasErrorsCount(final int expected) {
+			assertThat(errorCount).isEqualTo(expected);
+			assertThat(listener.getErrorEvents()).hasSize(expected);
+
+			return this;
+		}
+
+		public CheckAuditEventAssertion containsErrorAuditEventWithMessage(final String expected) {
+			assertThat(listener.getErrorEvents()).isNotEmpty();
+
+			for (final AuditEvent event : listener.getErrorEvents()) {
+				if (Objects.equals(event.getMessage(), expected)) {
+					return this;
+				}
+			}
+
+			throw new AssertionError(format("Audit listener not registered a error audit event with message: \"%s\"", expected));
+		}
+	}
+}
---
+++ src/test/java/ru/pva/code/style/checks/assertions/StubAuditListener.java
@@ -0,0 +1,49 @@
+package ru.pva.code.style.checks.assertions;
+
+import com.puppycrawl.tools.checkstyle.api.AuditEvent;
+import com.puppycrawl.tools.checkstyle.api.AuditListener;
+import java.util.ArrayList;
+import java.util.List;
+import javafx.util.Pair;
+import lombok.Getter;
+
+@Getter
+public class StubAuditListener implements AuditListener {
+
+	private final List<AuditEvent> auditStartedEvents = new ArrayList<>();
+	private final List<AuditEvent> auditFinishedEvents = new ArrayList<>();
+	private final List<AuditEvent> fileStartedEvents = new ArrayList<>();
+	private final List<AuditEvent> fileFinishedEvents = new ArrayList<>();
+	private final List<AuditEvent> errorEvents = new ArrayList<>();
+	private final List<Pair<AuditEvent, Throwable>> exceptionEvents = new ArrayList<>();
+
+	@Override
+	public void auditStarted(final AuditEvent auditEvent) {
+		auditStartedEvents.add(auditEvent);
+	}
+
+	@Override
+	public void auditFinished(final AuditEvent auditEvent) {
+		auditFinishedEvents.add(auditEvent);
+	}
+
+	@Override
+	public void fileStarted(final AuditEvent auditEvent) {
+		fileStartedEvents.add(auditEvent);
+	}
+
+	@Override
+	public void fileFinished(final AuditEvent auditEvent) {
+		fileFinishedEvents.add(auditEvent);
+	}
+
+	@Override
+	public void addError(final AuditEvent auditEvent) {
+		errorEvents.add(auditEvent);
+	}
+
+	@Override
+	public void addException(final AuditEvent auditEvent, final Throwable throwable) {
+		exceptionEvents.add(new Pair<>(auditEvent, throwable));
+	}
+}
